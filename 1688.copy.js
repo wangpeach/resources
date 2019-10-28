83b0a1203b7092365d3d954fe4cd44e4a9dccbb4
XTT\rjora <r.jorant@outlook.com>
2019-10-29 02:46:07 +0800

    1

--- 1688.copy.js
+++ 1688.copy.js
@@ -3,9 +3,9 @@ window.onload = function () {
     var copybtn = '<a class="distribute-to-cross-border" title="复制" rel="nofollow"></a>';
     var copybtn = document.createElement('a');
     copybtn.setAttribute('class', 'distribute-to-cross-border');
-    copybtn.innerHtml = '<span>复制</span>'
+    copybtn.innerHTML = '<span>复制</span>'
     copybtn.onclick = function() {
-        var title = document.getElementById("mod-detail-title").childNodes[0].innerText;
+        var title = document.getElementById("mod-detail-title").childNodes[0].innerTEXT;
         alert(title);
     }
 
