c9c6540cf88cb51a650fc035abf09d1e04a3063a
XTT\rjora <r.jorant@outlook.com>
2019-10-29 02:00:21 +0800

    .

---
+++ 1688.copy.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    alert("adsf");
+
+    setTimeout(() => {
+        var copybtn = $('<a class="distribute-to-cross-border" title="复制" rel="nofollow"><span>复制</span></a>');
+        $(".unit-detail-order-action").append(copybtn);
+    }, 1000);
+
+});
+
