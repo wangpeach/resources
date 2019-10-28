ac75dc5d22dcddaa6660990cf602a5188f6ea9b3
XTT\rjora <r.jorant@outlook.com>
2019-10-29 03:08:42 +0800

    .

--- 1688.copy.js
+++ 1688.copy.js
@@ -5,8 +5,19 @@ window.onload = function () {
     copybtn.setAttribute('class', 'distribute-to-cross-border');
     copybtn.innerHTML = '<span>复制</span>'
     copybtn.onclick = function() {
-        var title = document.getElementById("mod-detail-title").childNodes[0].innerTEXT;
-        alert(title);
+        var objs = {};
+
+        var title =  document.querySelector('#mod-detail-title>h1').innerText;
+        var imgprops = document.querySelectorAll("#dt-tab li img:not([data-lazy-src])");
+
+        var imgs = new Array();
+        imgprops.forEach(element => {
+            imgs.push(element.getAttribute('src'));
+        });
+
+        objs.title = title;
+        objs.imgs = imgs;
+        console.log(objs);
     }
 
     var action_panel = document.getElementsByClassName('unit-detail-order-action');
